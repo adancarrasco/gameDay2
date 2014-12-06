8cc97a8b2e53c445f8b35261d7c61c3d354f52e4
JuanCalvillo <juan.calvillo@outlook.com>
2014-12-06 14:34:47 -0600

    Mosca movible

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Draggable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            var pos = Input.mousePosition;
+            pos.z = 1;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            transform.position = pos;
+        }
+	}
+}
---
+++ Assets/Scripts/MoveFly.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFly : MonoBehaviour {
+
+    public SpriteRenderer flyLegs;
+
+	// Use this for initialization
+	void Start () {
+        Screen.showCursor = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var pos = Input.mousePosition;
+        pos.z = 1;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        transform.position = pos;
+
+        if (Input.GetMouseButton(0))
+        {
+            flyLegs.enabled = false;
+        }
+        else
+        {
+            flyLegs.enabled = true;
+        }
+	}
+}
